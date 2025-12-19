b8d3bb1e1948751e1fb36639fb66e49b9d45f963
Author <7813306+mehov@users.noreply.github.com>
2025-12-19 17:38:53 +0000

    Extend the form example

--- templates/Examples/self-service/form-contact/1-form.php
+++ templates/Examples/self-service/form-contact/1-form.php
@@ -1,14 +1,26 @@
 <?php
 $this->extend('self-service/form-contact/0-common');
 
-echo $this->Form->create(null);
+$entity = new \App\Model\Entity\Example();
+$entity->setError('address', ['Example Error: You forgot house number!']);
+
+echo $this->Form->create($entity);
 
 echo $this->Form->control('name', [
-    'label' => 'Name',
+    'label' => 'Full Name',
+    'help' => 'No special characters are allowed',
+]);
+
+echo $this->Form->control('sex', [
+    'type' => 'select',
+    'label' => 'Sex',
+    'options' => ['Male', 'Female'],
+    'help' => 'Example help text',
 ]);
 
 echo $this->Form->control('address', [
     'label' => 'Street and house number',
+    'help' => 'Make sure you do not forget the house number',
 ]);
 
 echo $this->Form->control('postal_code', [
@@ -28,12 +40,15 @@ echo $this->Form->control('email', [
 echo $this->Form->control('age_group', [
     'type' => 'radio',
     'label' => 'Age group',
+    'class' => 'test-class',
     'options' => ['Under 18', '18 – 29', '30 – 39', '40 – 59', 'Over 60'],
+    'help' => 'Example help text',
 ]);
 
 echo $this->Form->control('agree', [
-    'label' => 'Agree to Terms of Service',
     'type' => 'checkbox',
+    'label' => 'Agree to Terms of Service',
+    'help' => 'There is no way around this',
 ]);
 
 echo $this->Form->submit('Send');
